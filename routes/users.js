45db3a0c3af6eb33595bd7db094c9db3e4b330bb
DilanFdo <priyankarad276@gmail.com>
2024-11-09 19:50:14 +0530

    delete user

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 
 // Update user
 router.put("/:id", async (req, res) => {
-    if (req.body.userId === req.params.id || req.user?.isAdmin) { // Corrected typo here
+    if (req.body.userId === req.params.id || req.body.isAdmin) { // Corrected typo here
         if (req.body.password) {
             try {
                 const salt = await bcrypt.genSalt(10);
@@ -28,6 +28,19 @@ router.put("/:id", async (req, res) => {
     }
 }); 
 
-// Additional routes: delete user, get a user, follow users
+//delete user
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) { // Corrected typo here
+        
+        try {
+            const user = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Account deleted successfully");
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You can delete only your own account!");
+    }
+});
 
 module.exports = router; 
