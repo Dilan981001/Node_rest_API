71fa6e9f6d148ac830e0cb5ef85d1763b1b5394f
DilanFdo <priyankarad276@gmail.com>
2024-11-09 19:25:32 +0530

    register users

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,33 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get("/",(req,res)=>{
-    res.send("hey its user route")
-})
+// Update user
+router.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.user?.isAdmin) { // Corrected typo here
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch (err) {
+                return res.status(500).json(err);
+            }
+        }
+        try {
+            const user = await User.findByIdAndUpdate(
+                req.params.id,
+                { $set: req.body },
+                { new: true }
+            );
+            res.status(200).json("Account updated successfully");
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You can update only your own account!");
+    }
+}); 
 
-module.exports = router
+// Additional routes: delete user, get a user, follow users
+
+module.exports = router; 
