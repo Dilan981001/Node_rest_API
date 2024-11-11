cb847ac940e32195250ef713b6f4ede85f134953
DilanFdo <priyankarad276@gmail.com>
2024-11-11 09:15:57 +0530

    update post

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Post = require("../models/Post");
 
 //create post
-router.post("/", async (req, res) => {
+router.post("/", async (req,res) => {
     const newPost = new Post(req.body);
     try {
         const savePost = await newPost.save();
@@ -13,6 +13,20 @@ router.post("/", async (req, res) => {
 });
 
 //update post 
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.updateOne({ $set: req.body });
+            res.status(200).json("The post has been updated");
+        } else {
+            res.status(403).json("You can only update your own post");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 //delete post
 
